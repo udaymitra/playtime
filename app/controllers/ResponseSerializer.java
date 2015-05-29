0e84a178726337bfe03d1a0c311392d8bf16c471
pranav91690 <pranav91690@gmail.com>
2015-05-29 13:20:46 -0700

    response serializer

--- app/controllers/ResponseSerializer.java
+++ app/controllers/ResponseSerializer.java
@@ -1,7 +1,12 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 /**
  * Created by pranav on 5/29/15.
  */
 public class ResponseSerializer {
+    public static ObjectNode createJson(String message) {
+        return play.libs.Json.newObject().put("Message", message);
+    }
 }
