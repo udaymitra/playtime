b49391153738e59546864862f13ab433de42c56a
pranav91690 <pranav91690@gmail.com>
2015-05-28 20:32:43 -0700

    add view and js code

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,22 +2,25 @@ package controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import database.DatabaseQueryHandler;
 import database.SellerTableQueries;
 import models.EmailPassword;
 import models.LoginCredentials;
 import models.Seller;
+import play.api.libs.json.Json;
 import play.mvc.BodyParser;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+
 
 public class Application extends Controller {
     private static DatabaseQueryHandler databaseQueryHandler = new DatabaseQueryHandler();
     private static SellerTableQueries sellerTableQueries = new SellerTableQueries(databaseQueryHandler);
 
     public static Result index() {
-        return ok(index.render("Hi Pranav!"));
+        return ok(index.render("Hi Pranav Achanta!"));
     }
 
     @BodyParser.Of(BodyParser.Json.class)
@@ -26,7 +29,9 @@ public class Application extends Controller {
             EmailPassword emailPassword = RequestParser.getJsonFromRequest(request(), EmailPassword.class);
             LoginCredentials loginCredentials = new LoginCredentials(emailPassword, LoginCredentials.UserType.SELLER);
             boolean sellerCredentialsVerified = sellerTableQueries.checkSellerCredentials(loginCredentials);
-            return sellerCredentialsVerified ? ok("success") : ok("failure");
+            ObjectNode result = play.libs.Json.newObject();
+            result = sellerCredentialsVerified ? result.put("Message", "Success") : result.put("Message", "Failure");
+            return ok(result);
         } catch (Exception ex) {
             return ok("error handling query");
         }
@@ -36,7 +41,6 @@ public class Application extends Controller {
     public static Result addNewSeller() {
         try {
             Seller seller = RequestParser.getJsonFromRequest(request(), Seller.class);
-            System.out.println(seller);
             boolean addNewSellerSuccess = sellerTableQueries.addNewSeller(seller);
             return addNewSellerSuccess ? ok("success") : ok("failure");
         } catch (Exception ex) {
---
+++ public/javascripts/logpage.js
@@ -0,0 +1,131 @@
+$(document).ready(function() {
+	// Login Form Submission
+	$('#login').click(function(event){
+		// Remove Previous Errors If Any
+	    $('.form-group').removeClass('has-error'); 	// remove the error class
+	    $('.help-block').remove(); 					// remove the error text
+
+	    // Pick up Data from
+	    var formData = {
+	        'email'	            : $('#l-email').val(),
+	        'pass'              : $('#l-pass').val(),
+	    };
+
+		// Validations
+
+		// Email
+        if (!formData.email) {
+            $('#l-email-group').addClass('has-error'); // add the error class to show red input
+            $('#l-email-group').append('<div class="help-block"><p>Required</p></div>'); // add the actual error message under our input
+        }
+
+        // Password
+        if (!formData.pass) {
+            $('#l-pass-group').addClass('has-error'); // add the error class to show red input
+            $('#l-pass-group').append('<div class="help-block"><p>Required</p></div>'); // add the actual error message under our input
+         }
+
+
+		// The Ajax Call
+	    $.ajax({
+	        type        : 'POST', 	// define the type of HTTP verb we want to use (POST for our form)
+	        url         : 'http://localhost:9000/loginSeller', 		// the url where we want to POST
+	        data        : formData, // our data object
+	        dataType    : 'json', 	// what type of data do we expect back from the server
+	        encode      : true
+	    })
+	    // using the done promise callback
+	    .done(function(data) {
+	    	alert("Reaches Here");
+	    });
+
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+	});
+
+	// Login Form Submission
+	$('#register').click(function(event){
+		// Remove Previous Errors If Any
+	    $('.form-group').removeClass('has-error'); 	// remove the error class
+	    $('.help-block').remove(); 					// remove the error text
+
+	    // Pick up Data from
+	    var formData = {
+	    	'fname'				: $('#fname').val(),
+	    	'lname'				: $('#lname').val(),
+	        'email'	            : $('#email').val(),
+	        'pass'              : $('#pass').val(),
+	        'rpass'				: $('#rpass').val(),
+	        'zip'               : $('#zip').val(),
+	        'cus'               : $('#cus').val(),
+	    };
+
+		// Validations
+
+		// First Name
+        if (!formData.fname) {
+            $('#fname-group').addClass('has-error'); // add the error class to show red input
+            $('#fname-group').append('<div class="help-block"> <p>Required</p> </div>'); // add the actual error message under our input
+        }
+
+		// Last Name
+        if (!formData.lname) {
+            $('#lname-group').addClass('has-error'); // add the error class to show red input
+            $('#lname-group').append('<div class="help-block"> <p>Required</p> </div>'); // add the actual error message under our input
+        }
+
+		// Email
+        if (!formData.email) {
+            $('#email-group').addClass('has-error'); // add the error class to show red input
+            $('#email-group').append('<div class="help-block"> <p>Required</p> </div>'); // add the actual error message under our input
+        }
+
+        // Password
+        if (!formData.pass) {
+            $('#pass-group').addClass('has-error'); // add the error class to show red input
+            $('#pass-group').append('<div class="help-block"><p>Required</p></div>'); // add the actual error message under our input
+         }else{
+         	// Check	 for Password Compatibility
+         	// Empty Field
+         	if(!formData.rpass){
+         		$('#rpass-group').addClass('has-error'); // add the error class to show red input
+            	$('#rpass-group').append('<div class="help-block"><p>Required</p></div>'); // add the actual error message under our input
+         	}else{
+         		// Passwords Don't Match
+         		if (formData.pass != formData.rpass){
+         			$('#pass-group').addClass('has-error'); // add the error class to show red input
+            		$('#pass-group').append('<div class="help-block"><p>Passwords Don\'t Match</p></div>'); // add the actual error message under our input
+         		}
+         	}
+         }
+
+        // Zip Code
+        if (!formData.zip) {
+            $('#zip-group').addClass('has-error'); // add the error class to show red input
+            $('#zip-group').append('<div class="help-block"><p>Required</p></div>'); // add the actual error message under our input
+         }
+
+        // Cuisine
+        if (!formData.cus) {
+            $('#cus-group').addClass('has-error'); // add the error class to show red input
+            $('#cus-group').append('<div class="help-block"><p>Required</p></div>'); // add the actual error message under our input
+         }
+
+		// The Ajax Call
+	    $.ajax({
+	        type        : 'POST', // define the type of HTTP verb we want to use (POST for our form)
+	        url         : '', // the url where we want to POST
+	        data        : formData, // our data object
+	        dataType    : 'json', // what type of data do we expect back from the server
+	        encode      : true
+	    })
+	    // using the done promise callback
+	    .done(function(data) {
+
+	    });
+
+    // stop the form from submitting the normal way and refreshing the page
+    event.preventDefault();
+	});
+
+});
